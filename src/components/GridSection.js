aed9295e91fbe0be2e911f84b6250ac7984562c4
Lauren Burroughs <lauren.burroughs@contentful.com>
2021-10-06 18:05:14 -0500

    feat(sections): give sections ids for jump links

--- src/components/GridSection.js
+++ src/components/GridSection.js
@@ -19,8 +19,9 @@ export default class GridSection extends React.Component {
         let bg_img_repeat = _.get(section, 'background_image_repeat', null) || 'no-repeat';
         let grid_gap_x = _.get(section, 'grid_gap_horiz', null) || 'medium';
         let grid_gap_y = _.get(section, 'grid_gap_vert', null) || 'medium';
+				let sectionID = _.get(section, 'title').replace(/([^A-Za-z0-9[\]{}_.:-])\s?/g, '');
         return (
-            <section className={classNames('section', {'has-border': _.get(section, 'has_border', null), 'has-cover': _.get(section, 'background_image', null), 'bg-none': bg_color === 'none', 'bg-primary': bg_color === 'primary', 'bg-secondary': bg_color === 'secondary', 'pt-4': padding_top === 'small', 'pt-6': (padding_top === 'medium') || (padding_top === 'large'), 'pt-md-7': padding_top === 'large', 'pb-4': padding_bottom === 'small', 'pb-6': (padding_bottom === 'medium') || (padding_bottom === 'large'), 'pb-md-7': padding_bottom === 'large'})}>
+            <section id={sectionID} className={classNames('section', {'has-border': _.get(section, 'has_border', null), 'has-cover': _.get(section, 'background_image', null), 'bg-none': bg_color === 'none', 'bg-primary': bg_color === 'primary', 'bg-secondary': bg_color === 'secondary', 'pt-4': padding_top === 'small', 'pt-6': (padding_top === 'medium') || (padding_top === 'large'), 'pt-md-7': padding_top === 'large', 'pb-4': padding_bottom === 'small', 'pb-6': (padding_bottom === 'medium') || (padding_bottom === 'large'), 'pb-md-7': padding_bottom === 'large'})}>
             	{_.get(section, 'background_image', null) && (
             	<div className="cover-img" style={toStyleObj('background-image: url(\'' + withPrefix(_.get(section, 'background_image', null)) + '\'); opacity: ' + bg_img_opacity + '; background-size: ' + bg_img_size + '; background-repeat: ' + bg_img_repeat + '; background-position: ' + bg_img_position)}/>
             	)}
